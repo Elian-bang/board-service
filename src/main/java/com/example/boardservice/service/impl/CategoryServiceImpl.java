9f5f8c8595854327255c09f577a6013be90b9594
elian <daniel@mobidoc.us>
2025-02-24 00:58:46 +0900

    ENH:[#9] 로깅, 예외처리 개발
    1. log4j2 로깅 사용 및 기본 Spring로깅 라이브러리 비활성화
    2. 로깅 레벨 설정
    3. 로깅, 예외 처리

--- src/main/java/com/example/boardservice/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/boardservice/service/impl/CategoryServiceImpl.java
@@ -17,7 +17,12 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public void register(String accountId, CategoryDTO categoryDTO) {
         if(accountId != null) {
-            mapper.register(categoryDTO);
+            try {
+                mapper.register(categoryDTO);
+            }catch (RuntimeException e) {
+                log.error("register ERROR! {}", e.getMessage());
+                throw new RuntimeException("register ERROR! 게시글 카테고리 등록 메서드를 확인해주세요. " + e.getMessage());
+            }
         }else {
             log.error("register ERROR! {}", categoryDTO);
             throw new RuntimeException("register ERROR! 게시글 카테고리 등록 메서드를 확인해주세요. " + categoryDTO);
@@ -27,7 +32,12 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public void update(CategoryDTO categoryDTO) {
         if(categoryDTO != null) {
-            mapper.updateCategory(categoryDTO);
+            try {
+                mapper.updateCategory(categoryDTO);
+            }catch (RuntimeException e) {
+                log.error("update ERROR! {}", categoryDTO);
+                throw new RuntimeException("update ERROR! 게시글 카테고리 수정 메서드를 확인해주세요. " + categoryDTO);
+            }
         }else {
             log.error("update ERROR! {}", categoryDTO);
             throw new RuntimeException("update ERROR! 게시글 카테고리 수정 메서드를 확인해주세요. " + categoryDTO);
@@ -37,7 +47,12 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public void delete(int categoryId) {
         if (categoryId != 0) {
-            mapper.deleteCategory(categoryId);
+            try {
+                mapper.deleteCategory(categoryId);
+            }catch (RuntimeException e) {
+                log.error("delete ERROR! {}", categoryId);
+                throw new RuntimeException("delete ERROR! 게시글 카테고리 삭제 메서드를 확인해주세요. " + categoryId);
+            }
         } else {
             log.error("delete ERROR! {}", categoryId);
             throw new RuntimeException("delete ERROR! 게시글 카테고리 삭제 메서드를 확인해주세요. " + categoryId);
--- src/main/java/com/example/boardservice/service/impl/PostSearchServiceImpl.java
+++ src/main/java/com/example/boardservice/service/impl/PostSearchServiceImpl.java
@@ -2,11 +2,14 @@ package com.example.boardservice.service.impl;
 
 import com.example.boardservice.dto.PostDTO;
 import com.example.boardservice.dto.PostSearchRequest;
+import com.example.boardservice.exception.BoardServerException;
 import com.example.boardservice.mapper.PostSearchMapper;
 import com.example.boardservice.service.PostSearchService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +20,7 @@ public class PostSearchServiceImpl implements PostSearchService {
 
     @Autowired PostSearchMapper mapper;
 
+    @Async
     @Cacheable(value = "getPosts", key = "'getPosts' + #postSearchRequest.getName() + #postSearchRequest.getCategoryId()")
     @Override
     public List<PostDTO> getPosts(PostSearchRequest postSearchRequest) {
@@ -26,6 +30,7 @@ public class PostSearchServiceImpl implements PostSearchService {
             postList = mapper.selectPosts(postSearchRequest);
         } catch (Exception e) {
             log.error("getPost 메서드 실패: {}", e.getMessage());
+            throw new BoardServerException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
         return postList;
     }
@@ -37,6 +42,7 @@ public class PostSearchServiceImpl implements PostSearchService {
             postList = mapper.getPostByTag(tagName);
         }catch (RuntimeException e) {
             log.error("getPostByTag 메서드 실패: {}", e.getMessage());
+            throw new BoardServerException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
         return postList;
     }
